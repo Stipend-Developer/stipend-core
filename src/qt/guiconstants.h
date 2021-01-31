db6a51ce136511deb7f7f39a44beb4b056f6c422
CaseySPD <casey@stipend.me>
2021-01-31 21:37:29 +0000

    Update Data Directories and Binaries

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -53,9 +53,9 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 35
 
-#define QAPP_ORG_NAME "PIVX"
-#define QAPP_ORG_DOMAIN "pivx.org"
-#define QAPP_APP_NAME_DEFAULT "PIVX-Qt"
-#define QAPP_APP_NAME_TESTNET "PIVX-Qt-testnet"
+#define QAPP_ORG_NAME "Stipend"
+#define QAPP_ORG_DOMAIN "stipend.me"
+#define QAPP_APP_NAME_DEFAULT "Stipend-Qt"
+#define QAPP_APP_NAME_TESTNET "Stipend-Qt-testnet"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
