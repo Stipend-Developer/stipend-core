8007a4b93128c7471cbd87c28b53f9ede5183417
CaseySPD <casey@stipend.me>
2021-02-14 19:06:57 +0000

    Rebrand Text [Fix]

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -768,7 +768,7 @@ UniValue gettxout(const UniValue& params, bool fHelp)
             "     \"hex\" : \"hex\",        (string) \n"
             "     \"reqSigs\" : n,          (numeric) Number of required signatures\n"
             "     \"type\" : \"pubkeyhash\", (string) The type, eg pubkeyhash\n"
-            "     \"addresses\" : [          (array of string) array of pivx addresses\n"
+            "     \"addresses\" : [          (array of string) array of Stipend addresses\n"
             "     \"stipendaddress\"   	 	(string) stipend address\n"
             "        ,...\n"
             "     ]\n"
